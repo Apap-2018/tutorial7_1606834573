c80ccc0f52c25cb9b63267a80533636cae647196
ninanursita99 <ninanursita99@gmail.com>
2018-10-30 15:29:01 +0700

    done adding CarController for latihan1

--- src/main/java/com/apap/tutorial7/controller/CarController.java
+++ src/main/java/com/apap/tutorial7/controller/CarController.java
@@ -1,6 +1,7 @@
 package com.apap.tutorial7.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,93 +11,84 @@ import com.apap.tutorial7.service.CarService;
 import com.apap.tutorial7.service.DealerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * CarController
  */
-@Controller
+@RestController
+@RequestMapping("/car")
 public class CarController {
     @Autowired
     private CarService carService;
 
     @Autowired
-    private DealerService dealerService;
-
-    @RequestMapping(value = "/car/add/{dealerId}", method = RequestMethod.GET)
-    private String add(@PathVariable(value = "dealerId") Long dealerId, Model model) {
-        DealerModel dealer = dealerService.getDealerDetailById(dealerId).get();
-        dealer.setListCar(new ArrayList<CarModel>(){
-            private ArrayList<CarModel> init() {
-                this.add(new CarModel());
-                return this;
-            }
-        }.init());
-
-        model.addAttribute("dealer", dealer);
-        return "add-car";
+    RestTemplate restTemplate;
+    
+    @Bean
+    public RestTemplate rest() {
+    	return new RestTemplate();
     }
-
-    @RequestMapping(value="/car/add/{dealerId}", method = RequestMethod.POST, params={"addRow"})
-    public String addRow(@ModelAttribute DealerModel dealer, Model model) {
-        dealer.getListCar().add(new CarModel());
-
-        model.addAttribute("dealer", dealer);
-        return "add-car";
+    
+    @PutMapping(value = "/{id}")
+    private String updateCarSubmit (
+    		@PathVariable (value = "id") Long id,
+    		@RequestParam("brand") String brand,
+    		@RequestParam("type") String type,
+    		@RequestParam("price") Long price,
+    		@RequestParam("amount") Integer amount,
+    		@RequestParam("dealerId") Long dealerId) {
+    	CarModel car = (CarModel) carService.getCarDetailById(id).get();
+    	
+    	if(car.equals(null)) {
+    		return "Couldn't find your car";
+    	}
+    	
+    	car.setBrand(brand);
+    	car.setType(type);
+    	car.setPrice(price);
+    	car.setAmount(amount);
+    	
+    	carService.addCar(car);
+    	
+    	return "car update success";
     }
-
-    @RequestMapping(value="/car/add/{dealerId}", method = RequestMethod.POST, params={"removeRow"})
-    public String removeRow(@ModelAttribute DealerModel dealer, Model model, HttpServletRequest req) {
-        Integer rowId = Integer.valueOf(req.getParameter("removeRow"));
-        dealer.getListCar().remove(rowId.intValue());
-        
-        model.addAttribute("dealer", dealer);
-        return "add-car";
+    
+    @PostMapping(value = "/add")
+    private CarModel addCarSubmit(@RequestBody CarModel car) {
+    	carService.addCar(car);
+    	return car;
     }
-
-    @RequestMapping(value = "/car/add/{dealerId}", method = RequestMethod.POST, params={"save"})
-    private String addCarSubmit(@ModelAttribute DealerModel dealer) {
-        DealerModel archive = dealerService.getDealerDetailById(dealer.getId()).get();
-        for (CarModel car : dealer.getListCar()) {
-            if (car != null) {
-                car.setDealer(archive);
-                carService.addCar(car);
-            }
-        }
-        return "add";
+    
+    @GetMapping(value = "/{carId}")
+    private CarModel viewCar(@PathVariable ("carId") long carId, Model model) {
+    	return carService.getCarDetailById(carId).get();
     }
-
-    @RequestMapping(value = "/car/view", method = RequestMethod.GET)
-    private @ResponseBody CarModel view(@RequestParam(value = "type") String type, Model model) {
-        CarModel archive = carService.getCarDetailByType(type).get();
-        return archive;
+    
+    @GetMapping()
+    private List<CarModel> viewAllCar(Model model) {
+    	return carService.getAllCar();
     }
-
-    @RequestMapping(value = "/car/delete", method = RequestMethod.POST)
-    private String delete(@ModelAttribute DealerModel dealer, Model model) {
-        for (CarModel car : dealer.getListCar()) {
-            carService.deleteCar(car);
-        }
-        return "delete";
-    }
-
-    @RequestMapping(value = "/car/update", method = RequestMethod.GET)
-    private String update(@RequestParam(value = "type") String type, Model model) {
-        CarModel archive = carService.getCarDetailByType(type).get();
-        model.addAttribute("car", archive);
-        return "update-car";
-    }
-
-    @RequestMapping(value = "/car/update", method = RequestMethod.POST)
-    private @ResponseBody CarModel updatePilotSubmit(@ModelAttribute CarModel car, Model model) {
-        carService.addCar(car);
-        return car;
+    
+    @DeleteMapping(value = "/delete")
+    private String deleteCar(@RequestParam("carId") long id, Model Model) {
+    	CarModel car = carService.getCarDetailById(id).get();
+    	carService.deleteCar(car);
+    	return "Car has been deleted";
     }
 }
